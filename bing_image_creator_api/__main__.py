cbb33e6e694528f43e653e5dcd6b6f4d621b7ad9
megahomyak <g.megahomyak@gmail.com>
2023-12-03 05:01:23 +0500

    Saving and counting the test image too

--- bing_image_creator_api/__main__.py
+++ bing_image_creator_api/__main__.py
@@ -20,30 +20,33 @@ with open(args.token_file, encoding="utf-8") as f:
 tokens = list(filter(lambda s: s and not s.startswith("#"), lines))
 generations_amount = 0
 
-async def loop(token: str, token_number: int):
+async def create_and_save(token: str, token_number: int):
     global generations_amount
     async with aiohttp.ClientSession() as http_client:
         try:
-            while True:
-                if args.minimum_generations_amount and args.minimum_generations_amount <= generations_amount:
-                    break
-                try:
-                    urls = await create(token, args.prompt)
-                except UnsafeImageContentDetected:
-                    pass
-                else:
-                    generations_amount += len(urls)
-                    for url in urls:
-                        async with http_client.get(url) as file_response:
-                            image_name = urllib.parse.urlparse(url).path.rsplit("/", 1)[1]
-                            image_bytes = await file_response.read()
-                            file_extension = imghdr.what(None, h=image_bytes)
-                            file_name = f"{image_name}.{file_extension}"
-                            with open(file_name, "wb") as f:
-                                f.write(image_bytes)
-        except Exception:
-            print(f"Account number {token_number} (...{token[-6:]}) is giving up:")
-            traceback.print_exc()
+            urls = await create(token, args.prompt)
+        except UnsafeImageContentDetected:
+            pass
+        else:
+            generations_amount += len(urls)
+            for url in urls:
+                async with http_client.get(url) as file_response:
+                    image_name = urllib.parse.urlparse(url).path.rsplit("/", 1)[1]
+                    image_bytes = await file_response.read()
+                    file_extension = imghdr.what(None, h=image_bytes)
+                    file_name = f"{image_name}.{file_extension}"
+                    with open(file_name, "wb") as f:
+                        f.write(image_bytes)
+
+async def loop(token: str, token_number: int):
+    try:
+        while True:
+            if args.minimum_generations_amount and args.minimum_generations_amount <= generations_amount:
+                break 
+            await create_and_save(token, token_number)
+    except Exception:
+        print(f"Account number {token_number} (...{token[-6:]}) is giving up:")
+        traceback.print_exc()
 
 async def main():
     try:
